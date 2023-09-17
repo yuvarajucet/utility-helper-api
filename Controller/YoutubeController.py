5eb59b8f90d9a9edad978ee697928e18fcaca0b5
yuvaraj <yuvarajucet@gmail.com>
2023-09-17 14:47:25 +0530

    update auto delete files

--- Controller/YoutubeController.py
+++ Controller/YoutubeController.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from fastapi import APIRouter, Request, Response
 from fastapi.responses import FileResponse
 from Model.YoutubeData import YoutubeRequestModel
@@ -12,6 +13,7 @@ youtuber = APIRouter(
 
 @youtuber.post("/download", tags=["youtube"])
 async def download(request: Request, resp: Response, info: YoutubeRequestModel):
+    deleteOldItems()
     resp = YoutubeHelper.VideoDownloader(YoutubeHelper,info)
     return resp
 
@@ -28,3 +30,16 @@ async def download(type,url):
         "status": False,
         "message": "File not found"
     }
+
+def deleteOldItems():
+    folderPath = os.getcwd() + "/downloads/"
+    currentTime = datetime.datetime.now()
+    for filename in os.listdir(folderPath):
+        filePath = os.path.join(folderPath, filename)
+
+        if os.path.exists(filePath):
+            modifiedTime = datetime.datetime.fromtimestamp(os.path.getmtime(filePath))
+            timeDiff = currentTime - modifiedTime
+
+            if timeDiff.total_seconds() > 600:
+                os.remove(filePath)
